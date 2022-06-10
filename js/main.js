20442473e8cc79feedb07de858a88d90758e59dc
VuHuong0510 <104888617+bewildx24@users.noreply.github.com>
2022-06-10 23:56:46 +0700

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var arr = ['./images/v1.jpg','./images/v2.jpg','./images/v3.jpg','./images/v4.jpg','./images/v5.jpg']
+var index = 0
+
+function loadImages(){
+    document.getElementById('imgslide').src = arr[index]
+    index++
+    if(index == arr.length){
+        index = 0
+    }
+    setTimeout('loadImages()',1500)
+}
